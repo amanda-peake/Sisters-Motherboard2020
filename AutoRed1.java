abbecbdee958dab031e3f957524eb6fc9c6092d6
amanda-peake <46489999+amanda-peake@users.noreply.github.com>
2019-11-25 15:25:07 -0500

    Add files via upload

---
+++ AutoRed1.java
@@ -0,0 +1,66 @@
+/*package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+/*Created by mandy.peake and helen.watson on 11/22/19 */
+
+@Autonomous (name= "Red1", group= "Autonommous")
+public abstract class AutoRed1 extends LinearOpMode {
+    ElapsedTime runtime= new ElapsedTime();
+    HardwareRoverRuckus Bumblebarry = new HardwareRoverRuckus();
+
+    @Override
+    public void runOpMode () {
+        Bumblebarry.initializeRobot(hardwareMap);
+
+        waitForStart();
+
+        Bumblebarry.driveStraight(.5,5,1);
+    }
+
+}
+
+
+
+//drive forward
+
+
+//stops
+
+// turn
+
+//Vuforia Code to recognize star wars block
+
+//Use lift code and pick up block and place on builder zone
+
+//Turn
+
+//Park on foundation
